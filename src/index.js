07a57bf8668db942615b582c959121e85872bf64
Kelvin Guerrero <kelvinguerrero2199@outlook.es>
2024-05-02 14:47:27 +0200

    Add: auth system with jwt

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import v1EjercicioRouter from "./v1/routes/ejercicio.routes.js";
+import v1AuthRoutes from "./v1/routes/auth.routes.js";
 import cors from "cors";
 
 // Creamos el servidor
@@ -15,6 +16,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use("/api/v1/ejercicios", v1EjercicioRouter);
+app.use("/api/v1/auth", v1AuthRoutes);
 
 app.listen(port);
 console.log("Servidor iniciado en el puerto ", port);
--- src/models/model.js
+++ src/models/model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
 import { ejercicioSchema } from "./ejercicio.schema.js";
+import { userSchema } from "./user.model.js";
 
 export const ejercicioModel = mongoose.model("ejercicio", ejercicioSchema);
+export const userModel = mongoose.model("user", userSchema);
